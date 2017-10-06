732f8ff8af1fd7d2fe1d7156b537b567f0eae45e
Egor Fedorov <egor.fedorov@emlid.com>
2017-10-06 14:54:47 +0300

    setup.py: switch to bsd-3 license, version bump to 0.1.1, use find_packages

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="sysdmanager",
-    version="0.1.0",
-    license="GPL-3",
+    version="0.1.1",
+    license="BSD-3",
     author="Aleksandr Aleksandrov",
     author_email="aleksandr.aleksandrov@emlid.com",
     url="https://github.com/emlid/systemd-manager",
-    packages=["sysdmanager"]
+    packages=find_packages(exclude=["tests"])
 )
